b228abbbee2a8bafa72a043e8bf75c2b26ad5413
Levanza <kiki.manda16@gmail.com>
2026-01-13 09:27:59 +0700

    Remove time from payment detail screen, keep only date

--- lib/screens/payment_detail_screen.dart
+++ lib/screens/payment_detail_screen.dart
@@ -45,11 +45,9 @@ class PaymentDetailScreen extends StatelessWidget {
                       children: [
                         const Icon(Icons.calendar_today),
                         const SizedBox(width: 8),
-                        Expanded(
-                          child: Text(
-                            'Tanggal & Waktu: ${DateFormat('EEEE, dd MMMM yyyy \'pukul\' HH:mm', 'id_ID').format(payment.tanggalBayar)}',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
+                        Text(
+                          'Tanggal: ${DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(payment.tanggalBayar)}',
+                          style: Theme.of(context).textTheme.bodyLarge,
                         ),
                       ],
                     ),
